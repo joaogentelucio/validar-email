0a0c3324fae558ba34ee9199f06eff1f677e5a48
João Vitor <151798489+joaogentelucio@users.noreply.github.com>
2025-03-17 13:32:22 -0300

    ajuste 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import Login from './pages/SignIn'
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <Login />
   )
 }
 
+export default App;
